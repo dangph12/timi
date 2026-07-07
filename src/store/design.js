49a744ed36d8d16a620916aacd8783ade2d91694
Phan Hải Đăng <156905818+dangph12@users.noreply.github.com>
2026-07-07 09:14:57 +0700

    refactor: new designSelections shape (styleId + partId map) + partOptionsAtom

--- src/store/design.js
+++ src/store/design.js
@@ -1,19 +1,11 @@
 import { atom } from 'jotai';
 
 export const designSelectionsAtom = atom({
-  version: null,
-  hair: null,
-  eyes: null,
-  lip: null,
-  clothes: [],
-  accessory: [],
-  item: { type: null, color: null },
-  packaging: null
+  styleId: null,
+  selections: {}
 });
 
-export const canChooseClothesAtom = atom(
-  get => get(designSelectionsAtom).version !== 'economy'
-);
+export const partOptionsAtom = atom({});
 
 export const capturedCharacterAtom = atom(null);
 
