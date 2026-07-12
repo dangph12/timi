8b7d9c59b44d311d9feb8b1389310c372f60eac4
Phan Hải Đăng <156905818+dangph12@users.noreply.github.com>
2026-07-12 16:53:29 +0700

    feat: add sonner toast + Toaster to layout

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router";
 import Header from "@/components/header";
+import { Toaster } from "sonner";
 
 export default function Layout() {
   return (
@@ -8,6 +9,7 @@ export default function Layout() {
       <div className="flex-1 min-h-0">
         <Outlet />
       </div>
+      <Toaster />
     </div>
   );
 }
