36cddc23f6bc1323172f778b259b7a894f23c049
Phan Hải Đăng <156905818+dangph12@users.noreply.github.com>
2026-07-15 21:54:10 +0700

    feat: change url

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router";
-import Header from "@/components/Header";
+import Header from "./Header";
 import { Toaster } from "sonner";
 
 export default function Layout() {
